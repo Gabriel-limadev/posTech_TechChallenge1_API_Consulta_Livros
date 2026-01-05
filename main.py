599ac1775eee2fadca56506055e85bae24610586
Gabriel-limadev <gabrielvitor180701@gmail.com>
2026-01-05 16:14:59 -0300

    adicionando detalhes na rota principal: /api/v1

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ app = FastAPI(
     description="API de consulta de biblioteca de livros."
 )
 
+# Inclue um informativo na rota principal
+@app.get("/api/v1", tags=["Info"])
+def api_v1_root():
+    return {
+        "name": "Books API",
+        "version": "v1",
+        "docs": "/docs"
+    }
+
 app.include_router(api_v1_router, prefix="/api/v1")
